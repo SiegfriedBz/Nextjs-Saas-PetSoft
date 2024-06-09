49e2d257a1674c05b8a88b4cd2550459ae88fb0a
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-09 18:27:03 +0800

    add main folders + routes

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>HOME</main>
+  return <main>HOME Home</main>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Josefin_Sans } from 'next/font/google'
+import '@/styles/globals.css'
+import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const josefinSans = Josefin_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'PetSoft - Pet DayCare Software',
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`text-base text-zinc-900 bg-gradient-to-r from-zinc-200 to-zinc-50 ${josefinSans.className}`}
+      >
+        <Footer />
+        <main className='min-h-[100svh]'> {children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
