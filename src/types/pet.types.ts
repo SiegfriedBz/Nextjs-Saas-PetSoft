9c5711061f38fdf9bdfaad8ecf5f6ef8c1dd8988
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-10 13:55:09 +0800

    add pet type

---
+++ src/types/pet.types.ts
@@ -0,0 +1,12 @@
+export type TPet = {
+  id: string
+  name: string
+  imageUrl: string
+  age: number
+  breed: string
+  notes: string
+  ownerName: string
+  ownerPhoneNum: string
+  checkInDate: Date | string
+  checkOutDate: Date | string
+}
