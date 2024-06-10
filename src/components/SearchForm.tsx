0de96f47af4a2a0f1f7a675c22a3848c889f702b
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-10 19:23:09 +0800

    set petQuery from SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,12 +1,31 @@
 'use client'
 
+import usePets from '@/hooks/usePets'
+
 const SearchForm = () => {
+  const { petQuery, setPetQuery } = usePets()
+
   return (
     <form className='w-full h-full'>
       <input
-        className='w-full h-full placeholder:text-white/90 px-4 py-2 bg-white/20 rounded-md ring-1 ring-white border-none'
+        className='w-full h-full
+          placeholder:text-white/90
+          placeholder:focus:text-zinc-500/70
+          placeholder:hover:text-zinc-500/80
+          px-4 py-2
+          bg-white/20
+          hover:bg-white/30
+          rounded-md
+          ring-1 ring-white
+          outline-none
+          focus:bg-white/50
+          focus:text-zinc-600/90
+          transition duration-300
+        '
         placeholder='Search'
         type='search'
+        value={petQuery}
+        onChange={(e) => setPetQuery(e.target.value)}
       />
     </form>
   )
