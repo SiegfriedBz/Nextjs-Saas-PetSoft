11ff00d3da32156d69a3be453277c1edbbd7ca0c
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-10 19:20:18 +0800

    update ContentBlock

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -1,10 +1,18 @@
+import { twMerge } from 'tailwind-merge'
+
 type TProps = {
   children: React.ReactNode
+  className?: string
 }
 
-const ContentBlock = ({ children }: TProps) => {
+const ContentBlock = ({ children, className }: TProps) => {
   return (
-    <div className='h-full w-full bg-gradient-to-tr from-zinc-50 to-zinc-100 shadow-md shadow-zinc-200 rounded-md'>
+    <div
+      className={twMerge(
+        'h-full w-full bg-gradient-to-tr from-zinc-50 to-zinc-100 shadow-md shadow-zinc-200 rounded-md',
+        className
+      )}
+    >
       {children}
     </div>
   )
