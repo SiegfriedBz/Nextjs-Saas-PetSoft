78fc8295608b1c4d68f938b56801b53640f82bf2
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-10 19:21:28 +0800

    fix css

--- src/components/DashboardContentWrapper.tsx
+++ src/components/DashboardContentWrapper.tsx
@@ -21,20 +21,21 @@ const getData = async () => {
 // Server-Component
 const DashboardContentWrapper = async () => {
   const petsData = await getData()
+
   return (
     // Client-Component
     <PetsProvider petsData={petsData}>
-      <div className='grid max-md:grid-cols-1 md:grid-cols-[16rem_1fr] md:space-x-4'>
-        <section className='grid md:grid-rows-[2rem_1fr] max-md:space-y-2 md:space-y-4'>
+      <div className='grid max-md:grid-cols-1 md:grid-cols-[16rem_1fr] md:space-x-4 md:min-h-[62svh]'>
+        <section className='grid md:grid-rows-[2rem_1fr] max-md:space-y-2 md:h-full'>
           {/* Client-Component */}
           <SearchForm />
-          <ContentBlock>
+          <ContentBlock className='mt-4 ring-1 ring-white overflow-hidden'>
             {/* Client-Component */}
             <PetList />
           </ContentBlock>
         </section>
-        <section className='max-md:h-[34svh] md:h-[calc(100%+1rem)]'>
-          <ContentBlock>
+        <section className='max-md:min-h-[38svh] max-md:my-8 md:h-[calc(100%+1rem)]'>
+          <ContentBlock className='ring-1 ring-white overflow-hidden'>
             {/* Client-Component */}
             <PetDetails />
           </ContentBlock>
--- src/components/DashboardContentWrapperSkeleton.tsx
+++ src/components/DashboardContentWrapperSkeleton.tsx
@@ -5,14 +5,14 @@ import SearchFormSkeleton from '@/components/SearchFormSkeleton'
 
 const DashboardContentWrapperSkeleton = () => {
   return (
-    <div className='grid max-md:grid-cols-1 md:grid-cols-[16rem_1fr] md:space-x-4'>
+    <div className='grid max-md:grid-cols-1 md:grid-cols-[16rem_1fr] md:space-x-4 md:min-h-[62svh]'>
       <section className='grid md:grid-rows-[2rem_1fr] max-md:space-y-2 md:space-y-4'>
         <SearchFormSkeleton />
         <ContentBlock>
           <PetListSkeleton />
         </ContentBlock>
       </section>
-      <section className='max-md:h-[34svh] md:h-[calc(100%+1rem)]'>
+      <section className='max-md:min-h-[38svh] max-md:my-8 md:h-[calc(100%+1rem)]'>
         <ContentBlock>
           <PetDetailsSkeleton />
         </ContentBlock>
