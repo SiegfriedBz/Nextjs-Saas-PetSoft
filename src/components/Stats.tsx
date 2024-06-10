3c0d6aab5cb5f6647ae6a435e8bf3cd9e7810105
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-10 19:21:08 +0800

    update stats component

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,7 +1,26 @@
-const Stats = () => {
+import { TPet } from '@/types/pet.types'
+
+const getData = async () => {
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
+  )
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch pets data')
+  }
+
+  const petsData: TPet[] = await response.json()
+
+  return petsData
+}
+
+const Stats = async () => {
+  const petsData = await getData()
+  const numOfPets = petsData.length || 0
+
   return (
     <section className='text-center'>
-      <p className='text-2xl font-bold leading-6'>2</p>
+      <p className='text-2xl font-bold leading-6'>{numOfPets}</p>
       <p className='opacity-80'>Current guest</p>
     </section>
   )
