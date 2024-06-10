9ec0bece37736bbb6a4350614938702fb48bb197
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-10 13:52:58 +0800

    add app Account + Dashboard pages

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,8 +1,15 @@
+import Branding from '@/components/Branding'
+
 const Account = () => {
   return (
-    <div>
-      <h1>Account</h1>
-    </div>
+    <main className='space-y-4'>
+      <div className='flex justify-between items-center text-zinc-50'>
+        <Branding />
+        <section className='text-center'>
+          <p className='text-2xl font-bold leading-6'>Your Account</p>
+        </section>
+      </div>
+    </main>
   )
 }
 
--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,8 +1,23 @@
+import Branding from '@/components/Branding'
+import DashboardContentWrapper from '@/components/DashboardContentWrapper'
+import DashboardContentWrapperSkeleton from '@/components/DashboardContentWrapperSkeleton'
+
+import Stats from '@/components/Stats'
+import { Suspense } from 'react'
+
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <main className='space-y-4'>
+      <div className='flex justify-between items-center text-zinc-50'>
+        <Branding />
+        <Stats />
+      </div>
+
+      <Suspense fallback={<DashboardContentWrapperSkeleton />}>
+        {/* Server-Component */}
+        <DashboardContentWrapper />
+      </Suspense>
+    </main>
   )
 }
 
