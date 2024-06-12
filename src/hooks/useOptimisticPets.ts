ac3510c62395c01f1b4835d57db3c758794853d2
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-12 17:26:49 +0800

    update useOptimisticPets

--- src/hooks/useOptimisticPets.ts
+++ src/hooks/useOptimisticPets.ts
@@ -1,18 +1,32 @@
-import { checkOutPetAction } from '@/server-actions/checkOutPetAction'
+import { addPetAction } from '@/server-actions/addPetAction'
+import { deletePetAction } from '@/server-actions/deletePetAction'
+import { updatePetAction } from '@/server-actions/updatePetAction'
 import type { TPet } from '@/types/pet.types'
+import type { TCreatePetInput, TUpdatePetInput } from '@/zod/mutatePet.zod'
 import { useOptimistic } from 'react'
+import { toast } from 'sonner'
 
 const optimisticReducer = (
   state: TPet[],
-  { action, pet }: { action: string; pet: TPet }
+  {
+    action,
+    payload
+  }: { action: string; payload: TUpdatePetInput | TCreatePetInput | TPet['id'] }
 ) => {
   switch (action) {
     case 'optimisticCreate':
-      return [...state, pet]
-    case 'optimisticEdit':
-      return state.map((p) => (p.id !== pet.id ? p : pet))
-    case 'optimisticCheckout':
-      return state.filter(({ id }) => id !== pet.id)
+      const createPetInput = payload as TCreatePetInput & {
+        id: string
+      }
+      return [...state, createPetInput]
+    case 'optimisticUpdate':
+      const updatePetInput = payload as TUpdatePetInput
+      return state.map((pet) =>
+        pet.id !== updatePetInput.id ? pet : updatePetInput
+      )
+    case 'optimisticDelete':
+      const deletePetId = payload as TPet['id']
+      return state.filter((pet) => pet.id !== deletePetId)
     default:
       return state
   }
@@ -28,29 +42,59 @@ export const useOptimisticPets = ({ pets, handleSelectPet }: TProps) => {
     optimisticReducer
   )
 
-  const handleAddPet = async (pet: TPet) => {
+  const handleAddPet = async (createPetInput: TCreatePetInput) => {
     // set optimistic pets
-    setOptimisticPets({ action: 'optimisticCreate', pet })
-    // server-action TODO
+    setOptimisticPets({
+      action: 'optimisticCreate',
+      payload: {
+        ...createPetInput,
+        id: crypto.randomUUID()
+      }
+    })
+
+    // server-action
+    try {
+      const response = await addPetAction(createPetInput)
+      toast.success(`Pet added successfully`)
+      console.log('addPetAction response', response)
+    } catch (error) {
+      toast.error('Adding pet went wrong, please try again later.')
+      console.log('Pet ADD ERROR', error)
+    }
   }
 
-  const handleEditPet = async (pet: TPet) => {
+  const handleUpdatePet = async (updatePetInput: TUpdatePetInput) => {
     // set optimistic pets
-    setOptimisticPets({ action: 'optimisticEdit', pet })
-    // server-action TODO
+    setOptimisticPets({
+      action: 'optimisticUpdate',
+      payload: updatePetInput
+    })
+    // server-action
+    try {
+      const response = await updatePetAction(updatePetInput)
+      toast.success(`${updatePetInput.name} updated successfully`)
+      console.log('updatePetAction response', response)
+    } catch (error) {
+      toast.error('Updating pet went wrong, please try again later.')
+      console.log('Pet UPDATE ERROR', error)
+    }
   }
 
-  const handleCheckoutPet = async (pet: TPet) => {
-    console.log('==== handleCheckoutPet Checking out pet:', pet)
+  const handleDeletePet = async (petId: TPet['id']) => {
     // unselect pet
     handleSelectPet('')
     // set optimistic pets
-    setOptimisticPets({ action: 'optimisticCheckout', pet })
+    setOptimisticPets({ action: 'optimisticDelete', payload: petId })
     // server-action
-    const response = await checkOutPetAction(pet.id)
-    console.log('response', response)
-    console.log('Pet checked out successfully:', pet)
+    try {
+      const response = await deletePetAction(petId)
+      toast.success(`Pet deleted successfully`)
+      console.log('deletePetAction response', response)
+    } catch (error) {
+      toast.error('Deleting pet went wrong, please try again later.')
+      console.log('Pet DELETE ERROR', error)
+    }
   }
 
-  return { optimisticPets, handleAddPet, handleEditPet, handleCheckoutPet }
+  return { optimisticPets, handleAddPet, handleUpdatePet, handleDeletePet }
 }
