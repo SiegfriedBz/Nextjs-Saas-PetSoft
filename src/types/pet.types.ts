492c843044aef3cc018c868fdc770f697ce69f7b
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-12 17:23:33 +0800

    get TPet type from prisma schema

--- src/types/pet.types.ts
+++ src/types/pet.types.ts
@@ -1,12 +1,3 @@
-export type TPet = {
-  id: string
-  name: string
-  imageUrl: string
-  age: number
-  breed: string
-  notes: string
-  ownerName: string
-  ownerPhoneNum: string
-  checkInDate: Date | string
-  checkOutDate: Date | string
-}
+import { Pet as TPet } from '@prisma/client'
+
+export { type TPet }
