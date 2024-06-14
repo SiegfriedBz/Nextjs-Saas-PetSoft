56eda5f1b010665b9683caca78d9b2d3d2dd5804
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-14 19:00:26 +0800

    refactor + update types/inputs

--- src/hooks/useOptimisticPets.ts
+++ src/hooks/useOptimisticPets.ts
@@ -1,31 +1,31 @@
+'use client'
+
 import { addPetAction } from '@/server-actions/addPetAction'
 import { deletePetAction } from '@/server-actions/deletePetAction'
-import { updatePetAction } from '@/server-actions/updatePetAction'
-import type { TPet } from '@/types/pet.types'
-import type { TCreatePetInput, TUpdatePetInput } from '@/zod/mutatePet.zod'
+import updatePetAction from '@/server-actions/updatePetAction'
+import type { TPetEssentials } from '@/types/pet.types'
+import type { TMutatePetInput } from '@/zod/mutatePet.zod'
 import { useOptimistic } from 'react'
 import { toast } from 'sonner'
 
 const optimisticReducer = (
-  state: TPet[],
+  state: TPetEssentials[],
   {
     action,
     payload
-  }: { action: string; payload: TUpdatePetInput | TCreatePetInput | TPet['id'] }
-) => {
+  }: { action: string; payload: TMutatePetInput | TPetEssentials['id'] }
+): TPetEssentials[] => {
   switch (action) {
     case 'optimisticCreate':
-      const createPetInput = payload as TCreatePetInput & {
-        id: string
-      }
+      const createPetInput = payload as TMutatePetInput
       return [...state, createPetInput]
     case 'optimisticUpdate':
-      const updatePetInput = payload as TUpdatePetInput
+      const updatePetInput = payload as TMutatePetInput
       return state.map((pet) =>
         pet.id !== updatePetInput.id ? pet : updatePetInput
       )
     case 'optimisticDelete':
-      const deletePetId = payload as TPet['id']
+      const deletePetId = payload as TPetEssentials['id']
       return state.filter((pet) => pet.id !== deletePetId)
     default:
       return state
@@ -33,66 +33,77 @@ const optimisticReducer = (
 }
 
 type TProps = {
-  pets: TPet[]
+  pets: TPetEssentials[]
   handleSelectPet: (id: string) => void
 }
+
 export const useOptimisticPets = ({ pets, handleSelectPet }: TProps) => {
   const [optimisticPets, setOptimisticPets] = useOptimistic(
     pets,
     optimisticReducer
   )
 
-  const handleAddPet = async (createPetInput: TCreatePetInput) => {
-    // set optimistic pets
-    setOptimisticPets({
-      action: 'optimisticCreate',
-      payload: {
-        ...createPetInput,
-        id: crypto.randomUUID()
-      }
-    })
-
-    // server-action
+  const handleAddPet = async (createPetInput: TMutatePetInput) => {
     try {
+      // set optimistic pets
+      setOptimisticPets({
+        action: 'optimisticCreate',
+        payload: createPetInput // contains default pet id, needed to iterate over optimisticPets in PetList component
+      })
+
+      // server-action
       const response = await addPetAction(createPetInput)
-      toast.success(`Pet added successfully`)
-      console.log('addPetAction response', response)
+
+      if (response instanceof Error) {
+        throw new Error('Failed to add pet')
+      } else {
+        toast.success(`Pet ${response.name} added successfully`)
+      }
     } catch (error) {
       toast.error('Adding pet went wrong, please try again later.')
-      console.log('Pet ADD ERROR', error)
+      console.log('ADD PET ERROR', error)
     }
   }
 
-  const handleUpdatePet = async (updatePetInput: TUpdatePetInput) => {
-    // set optimistic pets
-    setOptimisticPets({
-      action: 'optimisticUpdate',
-      payload: updatePetInput
-    })
-    // server-action
+  const handleUpdatePet = async (updatePetInput: TMutatePetInput) => {
     try {
+      // set optimistic pets
+      setOptimisticPets({
+        action: 'optimisticUpdate',
+        payload: updatePetInput
+      })
+
+      // server-action
       const response = await updatePetAction(updatePetInput)
-      toast.success(`${updatePetInput.name} updated successfully`)
-      console.log('updatePetAction response', response)
+      if (response instanceof Error) {
+        throw new Error('Failed to update pet')
+      } else {
+        toast.success(`Pet ${response.name} updated successfully`)
+      }
     } catch (error) {
       toast.error('Updating pet went wrong, please try again later.')
-      console.log('Pet UPDATE ERROR', error)
+      console.log('UPDATE PET ERROR', error)
     }
   }
 
-  const handleDeletePet = async (petId: TPet['id']) => {
-    // unselect pet
-    handleSelectPet('')
-    // set optimistic pets
-    setOptimisticPets({ action: 'optimisticDelete', payload: petId })
-    // server-action
+  const handleDeletePet = async (petId: TPetEssentials['id']) => {
     try {
+      // unselect pet
+      handleSelectPet('')
+
+      // set optimistic pets
+      setOptimisticPets({ action: 'optimisticDelete', payload: petId })
+
+      // server-action
       const response = await deletePetAction(petId)
-      toast.success(`Pet deleted successfully`)
-      console.log('deletePetAction response', response)
+      if (response instanceof Error) {
+        throw new Error('Failed to delete pet')
+      } else {
+        toast.success(`Pet checked out successfully`)
+      }
     } catch (error) {
-      toast.error('Deleting pet went wrong, please try again later.')
-      console.log('Pet DELETE ERROR', error)
+      toast.error('Checking out pet went wrong, please try again later.')
+      console.log('DELETE PET ERROR', error)
     }
   }
 
