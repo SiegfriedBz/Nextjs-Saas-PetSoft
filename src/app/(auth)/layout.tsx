0eb82496ad90cf754d6afe73a99340bb45d196e1
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-14 18:44:21 +0800

    add auth pages layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import AppFooter from '@/components/AppFooter'
+import AppHeader from '@/components/AppHeader'
+import BackgroundPattern from '@/components/BackgroundPattern'
+import '@/styles/globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'PetSoft - Login',
+  description: 'Take care of people&apos;s pets responsibly with PetSoft.'
+}
+
+export default function AppLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <>
+      {/* Server-Component */}
+      <BackgroundPattern />
+      <main className='container flex flex-col mx-auto min-h-[100svh]'>
+        <div className='mt-16 w-full h-fit max-w-5xl mx-auto'>{children}</div>
+        {/* Server-Component */}
+        <AppFooter />
+      </main>
+    </>
+  )
+}
