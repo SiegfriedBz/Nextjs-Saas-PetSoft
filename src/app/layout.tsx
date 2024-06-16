05a74b784f3bcbd834bbdbd2703becad1d70b45b
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-16 12:09:20 +0800

    update og metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,23 @@ const josefinSans = Josefin_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'PetSoft - Pet DayCare Software',
-  description: 'Take care of people&apos;s pets responsibly with PetSoft.'
+  description: 'Take care of people&apos;s pets responsibly with PetSoft.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: process.env.NEXT_PUBLIC_URL,
+    siteName: 'PetSoft - Pet DayCare Software',
+    title: 'PetSoft - Pet DayCare Software',
+    description: 'Take care of people&apos;s pets responsibly with PetSoft.',
+    images: [
+      {
+        url: process.env.NEXT_PUBLIC_OG_IMAGE_URL as string,
+        width: 1200,
+        height: 630,
+        alt: 'PetSoft - Pet DayCare Software'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
@@ -19,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`text-base grid grid-rows-[1fr_max-content] text-zinc-900 bg-gradient-to-r from-zinc-200 to-zinc-50 ${josefinSans.className}`}
+        className={`text-base grid grid-rows-[1fr] text-zinc-900 bg-gradient-to-r from-zinc-200 to-zinc-50 ${josefinSans.className}`}
       >
         <main className='min-h-[100svh]'>
           <SessionProvider>{children}</SessionProvider>
